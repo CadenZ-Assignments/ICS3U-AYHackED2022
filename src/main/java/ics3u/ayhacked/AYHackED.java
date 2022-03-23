9f4eb65f4572988085593127b6adda46d923bb42
W1nter-isHere <W1nter.IsH3re@gmail.com>
2022-03-23 12:01:02 -0400

    Setup

---
+++ src/main/java/ics3u/ayhacked/AYHackED.java
@@ -0,0 +1,11 @@
+package ics3u.ayhacked;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(AYHackED.MODID)
+public class AYHackED {
+    public static final String MODID = "ayhacked";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+}
